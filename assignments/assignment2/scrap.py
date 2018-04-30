3df026d3f15d8ef43caed991c18efad4f650a238
CORP\kazit <tanzil.kazi@gmail.com>
2018-05-01 01:27:53 +1000

    middle of experiment1

--- assignments/assignment2/scrap.py
+++ assignments/assignment2/scrap.py
@@ -12,7 +12,8 @@ import time
 
 time_taken = 0
 
-def LR_baseline(corpus):
+#LR_baseilne
+def experiment0(corpus):
     # combine title and body into one column
     corpus["title_body"] = corpus["title"] + " " + corpus["body"]
     corpus = corpus.drop(["title", "body"], axis=1)
@@ -27,10 +28,28 @@ def LR_baseline(corpus):
     print("input sparsity ratio: {:0.1f}%".format(sparsity_ratio(corpus_np)*100))
     return corpus_vec
 
+def experiment1(corpus):
+    # combine title and body into one column
+    # corpus["title_body"] = corpus["title"] + " " + corpus["body"]
+    # corpus = corpus.drop(["title", "body"], axis=1)
+
+    # lower case and remove punctuations
+    corpus["title"] = corpus["title"].str.lower()
+    corpus["title"] = corpus["title"].str.replace('[{}]'.format(string.punctuation), '')
+
+    vec = CountVectorizer(ngram_range=(2, 2))
+    corpus_vec = vec.fit_transform(corpus["title"])
+
+    corpus_np = corpus_vec.toarray()
+    print("input sparsity ratio: {:0.1f}%".format(sparsity_ratio(corpus_np) * 100))
+    return corpus_vec
+
 def train_and_test_LR(X,Y):
     LR = LogisticRegressionCV(Cs=10,penalty="l2",solver="sag",multi_class="multinomial",
-                              scoring="accuracy",refit=True,n_jobs=7,max_iter=5)
-    print(cross_val_score(LR,X,Y,cv=10,n_jobs=7,scoring="accuracy"))
+                              scoring="accuracy",refit=True,n_jobs=3,max_iter=5)
+    accuracy = cross_val_score(LR,X,Y,cv=10,n_jobs=3,scoring="accuracy")
+    print("max accuracy: {:0.1f}%".format(max(accuracy)*100))
+    print("max mean: {:0.1f}%".format(np.mean(accuracy)*100))
     return LR
 
 def sparsity_ratio(X):
@@ -51,8 +70,8 @@ if __name__ == "__main__":
 
     corpus = topic_file
 
-    corpus_formatted = LR_baseline(corpus)
-
+    corpus_formatted = experiment1(corpus)
+    print("Shape of vectorized data:",corpus_formatted.shape)
     elapsed_time("Pre-processing data")
 
     LR = train_and_test_LR(corpus_formatted,corpus["annotation"])
@@ -64,7 +83,8 @@ if __name__ == "__main__":
 
     output = topic_file
     output["results"] = pd.Series(results)
-    output.drop(["title_body"],axis=1)
+    if "title_body" in output.columns:
+        output.drop(["title_body"],axis=1)
     output.to_csv("predictions.csv")
 
 
