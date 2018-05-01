fa7354c78d7aee86bfc0c38a37c474f7114a144e
CORP\kazit <tanzil.kazi@gmail.com>
2018-05-01 23:56:05 +1000

    updated framework to remove LRCV, loop over cross_val_score and produce proper output with training and test at 90:10 experiment 1 needs to be completed

--- assignments/assignment2/scrap.py
+++ assignments/assignment2/scrap.py
@@ -5,25 +5,29 @@ import numpy as np
 import string
 from scipy import sparse
 from sklearn.model_selection import KFold
-from sklearn.linear_model import LogisticRegressionCV
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
+from sklearn.model_selection import ShuffleSplit
 import time
+import warnings
 
+warnings.filterwarnings("ignore")
 time_taken = 0
 
 #LR_baseilne
-def experiment0(corpus):
+def experiment0(data):
     # combine title and body into one column
-    corpus["title_body"] = corpus["title"] + " " + corpus["body"]
-    corpus = corpus.drop(["title", "body"], axis=1)
+    data["title_body"] = data["title"] + " " + data["body"]
+    data = data.drop(["title", "body"], axis=1)
 
     # lower case and remove punctuations
-    corpus["title_body"] = corpus["title_body"].str.lower()
-    corpus["title_body"] = corpus["title_body"].str.replace('[{}]'.format(string.punctuation), '')
+    data["title_body"] = data["title_body"].str.lower()
+    data["title_body"] = data["title_body"].str.replace('[{}]'.format(string.punctuation), '')
 
     vec = CountVectorizer()
-    corpus_vec = vec.fit_transform(corpus["title_body"])
+    corpus_vec = vec.fit_transform(data["title_body"])
     corpus_np = corpus_vec.toarray()
     print("input sparsity ratio: {:0.1f}%".format(sparsity_ratio(corpus_np)*100))
     return corpus_vec
@@ -44,14 +48,6 @@ def experiment1(corpus):
     print("input sparsity ratio: {:0.1f}%".format(sparsity_ratio(corpus_np) * 100))
     return corpus_vec
 
-def train_and_test_LR(X,Y):
-    LR = LogisticRegressionCV(Cs=10,penalty="l2",solver="sag",multi_class="multinomial",
-                              scoring="accuracy",refit=True,n_jobs=3,max_iter=5)
-    accuracy = cross_val_score(LR,X,Y,cv=10,n_jobs=3,scoring="accuracy")
-    print("max accuracy: {:0.1f}%".format(max(accuracy)*100))
-    print("max mean: {:0.1f}%".format(np.mean(accuracy)*100))
-    return LR
-
 def sparsity_ratio(X):
     return 1.0 - np.count_nonzero(X) / float(X.shape[0] * X.shape[1])
 
@@ -60,32 +56,44 @@ def elapsed_time(s):
     print(s,": {:0.3f}s".format(time.clock() - time_taken))
     time_taken = time.clock()
 
-
 if __name__ == "__main__":
     time_taken = time.clock()
 
     # import input data
     topic_file = pd.read_csv("topic.csv")
     virality_file = pd.read_csv("virality.csv")
+    corpus = topic_file.drop(["annotation"],axis=1,inplace=False)
+    corpus_annotation = topic_file["annotation"]
 
-    corpus = topic_file
-
-    corpus_formatted = experiment1(corpus)
+    corpus_formatted = experiment0(corpus)
     print("Shape of vectorized data:",corpus_formatted.shape)
     elapsed_time("Pre-processing data")
 
-    LR = train_and_test_LR(corpus_formatted,corpus["annotation"])
+    accuracy_vec = []
+    LR = LogisticRegression(penalty="l2", solver="sag", multi_class="multinomial",
+                            warm_start=True, n_jobs=-1, max_iter=20)
+    cv = ShuffleSplit(n_splits=10, train_size=0.9, test_size=None)
+    num_loops = 0
+    while (num_loops < 5):
+        accuracy = cross_val_score(LR, corpus_formatted, corpus_annotation, cv=cv, n_jobs=-1, scoring="accuracy")
+        accuracy_vec.append(np.mean(accuracy) * 100)
+        num_loops = num_loops + 1
+
+    print("Mean accuracy: {:0.1f} +- {:0.1f}".format(np.mean(accuracy_vec),np.std(accuracy_vec)))
     elapsed_time("To train and evaluate")
 
-    model = LR.fit(corpus_formatted,corpus["annotation"])
-    results = model.predict(corpus_formatted)
+    train_X, test_X, train_Y, test_Y = train_test_split(corpus,corpus_annotation,train_size=0.9,random_state=2)
+    corpus_sparse = experiment0(corpus)
+    model = LR.fit(corpus_sparse[train_X.index.tolist()],train_Y)
+    results = model.predict(corpus_sparse[test_X.index.tolist()])
     elapsed_time("Prediction complete")
 
-    output = topic_file
-    output["results"] = pd.Series(results)
-    if "title_body" in output.columns:
-        output.drop(["title_body"],axis=1)
-    output.to_csv("predictions.csv")
+    output_df = pd.DataFrame(corpus.iloc[test_X.index.tolist(),])
+    output_df["annotation"] = corpus_annotation[test_X.index.tolist()]
+    output_df["results"] = pd.Series(results,index=test_X.index.tolist())
+    if "title_body" in output_df.columns:
+        output_df.drop(["title_body"],axis=1)
+    output_df.to_csv("predictions.csv")
 
 
     # for train_index,test_index in Kf.split(corpus_np):
@@ -96,26 +104,4 @@ if __name__ == "__main__":
     #     i = i + 1
 
 
-    # #tester
-    # d = {'id':[1,2,3,4,5,6,7,8,9,10],
-    #      'str':["today is sunday",
-    #             "nice! weather is is great",
-    #             "sunday funday",
-    #             "what's happening tomorrow?",
-    #             "got a meeting on Monday",
-    #             "when's the next public holiday?",
-    #             "Wednesday",
-    #             "let us meet tomorrow",
-    #             "it will rain tomorrow",
-    #             "tomorrow is Monday"],
-    #      "cat":["A","A","A","B","A","B","B","B","B","A"]}
-    #
-    # df = pd.DataFrame(data=d)
-    # vec = CountVectorizer()
-    # #print(df)
-    # x = vec.fit_transform(df["str"])
-    # #print(x.toarray())
-    # Kf = KFold(n_splits=3,shuffle=True)
-    # for train_index,test_index in Kf.split(df):
-    #     print("TRAIN:", train_index, "TEST:", test_index)
-    #     print(df["cat"][test_index])
+
---
+++ assignments/assignment2/tester.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import KFold
+
+#tester
+d = {'id':[1,2,3,4,5,6,7,8,9,10],
+     'str':["today is sunday",
+            "nice! weather is is great",
+            "sunday funday",
+            "what's happening tomorrow?",
+            "got a meeting on Monday",
+            "when's the next public holiday?",
+            "Wednesday",
+            "let us meet tomorrow",
+            "it will rain tomorrow",
+            "tomorrow is Monday"],
+     "cat":["A","A","A","B","A","B","B","B","B","A"]}
+
+df = pd.DataFrame(data=d)
+# vec = CountVectorizer()
+# x = vec.fit_transform(df["str"])
+# Kf = KFold(n_splits=3,shuffle=True)
+# for train_index,test_index in Kf.split(df):
+#     print("TRAIN:", train_index, "TEST:", test_index)
+#     print(df["cat"][test_index])
+#
+
+array = [60,61,34,65,90,23,60,60]
+print(np.std(array))
